15f27eb40dcb3d14b2321c6192cbe4210665b7b7
Pedro Amorim <pedro.amorim@ptfs-europe.com>
2024-12-10 14:26:54 -0100

    populate incdocs_id library additional fields

--- Koha/Plugin/Com/PTFSEurope/IncDocs/misc4dev/populate_required_fields.pl
+++ Koha/Plugin/Com/PTFSEurope/IncDocs/misc4dev/populate_required_fields.pl
@@ -4,6 +4,8 @@ use C4::Context;
 
 use Koha::Patron::Attribute::Type;
 use Koha::Patron::Attributes;
+use Koha::Patron::Attributes;
+use Koha::AdditionalField;
 
 my $ill_partner_category = C4::Context->preference('ILLPartnerCode') || 'IL';
 
@@ -15,6 +17,17 @@ try {
             class         => 'IncDocs',
             unique_id     => 1,
             category_code => $ill_partner_category
+        }
+    )->store;
+} catch {
+    print "Error: $_\n";
+};
+
+try {
+    Koha::AdditionalField->new(
+        {
+            tablename      => 'branches',
+            name           => 'incdocs_id',
         }
     )->store;
 } catch {
