eb94fbffc011117e46db7249c076179f42b35f4e
Pedro Amorim <pedro.amorim@openfifth.co.uk>
2025-12-10 15:17:09 -0100

    version bump

--- Koha/Plugin/Com/PTFSEurope/IncDocs.pm
+++ Koha/Plugin/Com/PTFSEurope/IncDocs.pm
@@ -39,13 +39,13 @@ use Koha::ILL::Request::Workflow;
 use Koha::Libraries;
 use Koha::Patrons;
 
-our $VERSION = "2.5.1";
+our $VERSION = "2.5.2";
 
 our $metadata = {
     name            => 'IncDocs',
     author          => 'Open Fifth',
     date_authored   => '2024-11-18',
-    date_updated    => "2025-11-21",
+    date_updated    => "2025-12-10",
     minimum_version => '25.05.00.000',
     maximum_version => undef,
     version         => $VERSION,
